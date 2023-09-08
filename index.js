73df77967162c81bf312dd3507bd0f607068e533
Thunder <nonthacha.h@gmail.com>
2023-09-08 23:36:54 +0700

    update front

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const  express = require('express');
+const path = require("path");
+
 const  app = express();
 const http = require('http');
-const server = http.createServer(app);
+const server = require("http").createServer(app);
 
 const { Server } = require("socket.io");
 const io = new Server(server); 
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html'); 
+app.use(express.static(path.join(__dirname + '/public')))
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname + '/index.html'); 
+// });
+
+io.on('connection', function(socket){
+  socket.on("newuser",function(username){
+    socket.broadcast.emit("update", username + " joined the conversation");
+  });
+  socket.on("exituser",function(username){
+    socket.broadcast.emit("update", username + " left the conversation");
+  });
+  socket.on("chat",function(username){
+    socket.broadcast.emit("chat", message);
+  });
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-      });
-  });
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected');
+//     });
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//         io.emit('chat message', msg);
+//       });
+//   });
 
 server.listen(3000, ()=>{
     console.log('listening on port 3000');
